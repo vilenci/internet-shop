04f529d32ff8262146dd109e22610ef86394c0ac
vilenci <45372550+vilenci@users.noreply.github.com>
2019-07-25 22:24:19 +0300

    Тест проверки наличия кнопки добавить в корзину

---
+++ test_items.py
@@ -0,0 +1,7 @@
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_all_language_see_button(browser):
+    browser.get(link)
+    browser.find_element_by_css_selector(".btn-add-to-basket")
