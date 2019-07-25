eca4bf2ab50a93294aa0ed7534390a84a79e911a
vilenci <45372550+vilenci@users.noreply.github.com>
2019-07-25 22:31:38 +0300

    Изменение названия фукции

--- test_items.py
+++ test_items.py
@@ -2,6 +2,6 @@
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_all_language_see_button(browser):
+def test_all_language_see_button_add_to_basket(browser):
     browser.get(link)
     browser.find_element_by_css_selector(".btn-add-to-basket")
